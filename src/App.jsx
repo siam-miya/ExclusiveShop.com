cff5847ac7533a8923444c700d7f9f9930c3dd7b
siam-miya <siammiya2024@gamil.com>
2026-01-22 23:27:01 +0600

    project setup  done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline bg-amber-800">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App
